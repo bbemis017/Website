7bb221978cf058203848073d95cf8d3dc5c9f627
root <root@ip-172-31-27-181.ec2.internal>
2016-05-22 05:31:19 +0000

    phalcon setup

---
+++ setup_phalcon.sh
@@ -0,0 +1,9 @@
+apt-get install php5 php5-dev php5-mysql gcc apache2 git
+apt-get install libpcre3-dev libapache2-mod-php5
+git clone --depth=1 git://github.com/phalcon/cphalcon.git ~/
+cd ~/cphalcon/ext
+sudo ./install
+echo "extension=/usr/lib/php5/20121212/phalcon.so" > /etc/php5/mods-available/phalcon.ini
+ln -s /etc/php5/mods-available/phalcon.ini /etc/php5/apache2/conf.d/20-phalcon.ini
+ln -s /etc/php5/mods-available/phalcon.ini /etc/php5/cli/conf.d/20-phalcon.ini
+service apache2 restart
