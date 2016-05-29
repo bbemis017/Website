ef83d4ebc2d0b71bfe59717c2b99ffde2bbf5bc6
Benjamin Bemis <bbemis@purdue.edu>
2016-05-29 18:35:25 -0400

    deploy livestock app

--- scripts/update_website.sh
+++ scripts/update_website.sh
@@ -1,3 +1,9 @@
+#update website code
 git pull origin master
+
+#update configuration files
 cp ../config/000-default.conf.apache /etc/apache2/sites-available/000-default.conf
+cp ../config/ports.conf.apache /etc/apache2/ports.conf
+
+#restart apache
 service apache2 restart
