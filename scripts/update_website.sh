e509b981507e89dfba78a0f948f7b69a9ef657ae
aws server <bbemis@purdue.edu>
2016-05-30 01:52:25 +0000

    update configurations

--- scripts/update_website.sh
+++ scripts/update_website.sh
@@ -4,6 +4,7 @@ git pull origin master
 #update configuration files
 cp ../config/000-default.conf.apache /etc/apache2/sites-available/000-default.conf
 cp ../config/ports.conf.apache /etc/apache2/ports.conf
+cp ../config/apache2.conf.apache /etc/apache2/apache2.conf
 
 #restart apache
 service apache2 restart
