f7d4a1c954d6473fd66e78493999dd5b87a074cc
Benjamin Bemis <bbemis@purdue.edu>
2016-06-05 00:16:35 -0400

    new configuration using apache aliases, mysql, phpmyadmin

---
+++ scripts/mysql_install.sh
@@ -0,0 +1,8 @@
+#install mysql
+apt-get install mysql-server php5-mysql
+mysql_install_db
+mysql_secure_installation
+
+#install phpmyadmin
+apt-get install phpmyadmin
+php5enmod mcrypt
--- scripts/newApp.sh
+++ scripts/newApp.sh
@@ -1,11 +1,12 @@
 APP_NAME=$1
-GITHUB=$2
+#GITHUB=$2
 MAX_APPS=20
 
 #check argument list
-if [ $# -eq 2 ]
+if [ $# -lt 2 ]
 then
 	echo 'invalid argument list'
+	exit
 fi
 
 #Get number of active apps
@@ -16,38 +17,36 @@ APPS=`expr $APPS - 1`
 if [ "$APPS" -ge "$MAX_APPS" ]
 then
 	echo 'Too many ports in use, check aws security group'
+	exit
 fi
-
-#clone github directory
-cd ../..
-git clone $GITHUB
-
-#setup
-cd $APP_NAME/scripts
-sh update.sh
-cd ../../Website/scripts
-
-PORT=`expr 80 + $APPS - 1`
-echo "port is: $PORT"
 
 #update app list
 APP_LIST=../config/apps.txt
 echo "$APP_NAME" >> $APP_LIST
 
-#add info to 000-default.conf.apache
+#copy contents of prepend file to 000-default.conf.apache
+PREPEND=../config/000-default.conf.prepend
 VIRTUAL_HOST=../config/000-default.conf.apache
-echo "<VirtualHost *:$PORT>" >> $VIRTUAL_HOST
-echo "	ServerAdmin webmaster@localhost" >> $VIRTUAL_HOST
-echo "	DocumentRoot /var/www/$APP_NAME/$APP_NAME/public/" >> $VIRTUAL_HOST
-echo "	ServerName $APP_NAME" >> $VIRTUAL_HOST
+cp $PREPEND $VIRTUAL_HOST
+
+#create an alias for every app
+while read -r line
+do
+	#do this for every app
+	DIR=/var/www/$APP_NAME/$APP_NAME/public
+	echo "Alias /$APP_NAME $DIR" >> $VIRTUAL_HOST
+	echo "<Directory $DIR>" >> $VIRTUAL_HOST
+	echo "	Options FollowSymLinks" >> $VIRTUAL_HOST
+	echo "	DirectoryIndex index.php" >> $VIRTUAL_HOST
+	echo " 	AllowOverride all" >> $VIRTUAL_HOST
+	echo "</Directory>" >> $VIRTUAL_HOST
+
+done < "$APP_LIST"
+
 echo "</VirtualHost>" >> $VIRTUAL_HOST
 
-#add info to ports.conf.apache
-CONFIG_PORTS=../config/ports.conf.apache
-echo "Listen $PORT" >> $CONFIG_PORTS
-
 #update apache files
-cp ../config/ports.conf.apache /etc/apache2/ports.conf
 cp ../config/apache2.conf.apache /etc/apache2/apache2.conf
+cp $VIRTUAL_HOST /etc/apache2/sites-available/000-default.conf
 
 service apache2 restart
