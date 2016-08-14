d5fc04166d32c9b22e158c277030f8010f5bd34d
Ben Bemis <bbemis@purdue.edu>
2016-08-14 13:47:16 -0400

    after deploy fixes

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,8 +3,12 @@ apt-get install apache2
 
 #setup framework and db
 #sh setup_phalcon.sh
-sh mysql_install.sh 
-sh install_php7.sh 
+sh mysql_install.sh
+sh install_php7.sh
+
+#required for phpmyadmin
+apt-get install php-mbstring php7.0-mbstring
+phpenmod mbstring
 
 #update code and config files
 sh update_website.sh
