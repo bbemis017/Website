386378e18af453f4bdf46c6b6714d0fc7cc06bea
Ben Bemis <bbemis@purdue.edu>
2016-08-14 12:56:49 -0400

    ready for deployment

--- scripts/newApp.sh
+++ scripts/newApp.sh
@@ -26,6 +26,7 @@ echo "$APP_NAME" >> $APP_LIST
 
 #copy contents of prepend file to 000-default.conf.apache
 PREPEND=../config/000-default.conf.prepend
+APPEND=../config/000-default.conf.append
 VIRTUAL_HOST=../config/000-default.conf.apache
 cp $PREPEND $VIRTUAL_HOST
 
@@ -38,21 +39,22 @@ do
 	do
 		echo "    " $line2 >> $VIRTUAL_HOST
 	done < $APACHE
-	
+
 	echo " " >> $VIRTUAL_HOST
 
 done < "$APP_LIST"
 
-echo "</VirtualHost>" >> $VIRTUAL_HOST
+#echo "</VirtualHost>" >> $VIRTUAL_HOST
+cat $APPEND >> $VIRTUAL_HOST
 
 #update apache files
 cp ../config/apache2.conf.apache /etc/apache2/apache2.conf
 cp $VIRTUAL_HOST /etc/apache2/sites-available/000-default.conf
 
 #update repository on origin master
-git add $APP_LIST
-git commit -m "Configured $APP_NAME on server"
-git push origin master
-echo "commited changes to origin master"
+#git add $APP_LIST
+#git commit -m "Configured $APP_NAME on server"
+#git push origin master
+#echo "commited changes to origin master"
 
 service apache2 restart
